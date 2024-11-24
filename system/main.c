e02fd0f8ac92a3b716473a2abfd8c995f0e9d59e
Haoyu Jin <jin511@purdue.edu>
2024-11-24 12:17:49 -0500

    Update a stronger version of benchmark, but still only for 2ind node, though the logic is similar as 3ind

--- system/main.c
+++ system/main.c
@@ -5,6 +5,7 @@
 #define CONTENT "Hello World"
 #define LEN 11
 #define ADDITION 512 * 3 + 11
+#define NUM_WRITTEN 888888
 
 process main() {
 	// print some constants
@@ -16,41 +17,59 @@ process main() {
 
 	lifscreate(RAM0, 128, 2000000); // initialize the ramdisk
 
-	did32 fd;
+	did32 fd1, fd2;
 	int status;
-	int i;
+	int i, len1, len2;
+	len1 = len2 = 0;
 
 	// open the file for write
 
-	fd = open(LIFILESYS, "index.txt", "rw");
-	kprintf("fd: %d\n", fd);
+	fd1 = open(LIFILESYS, "index.txt", "rw");
+	fd2 = open(LIFILESYS, "content.txt", "rw");
+	kprintf("fds: %d %d\n", fd1, fd2);
 
 	// test putc
-	for (i = 0; i < 888888; i++){
-		status = putc(fd, 'A' + (i % 25));
+	for (i = 0; i < NUM_WRITTEN * 2; i++){
+		if ((rand() & 1) && (len1 < NUM_WRITTEN)){
+			status = putc(fd1, 'A' + (len1 % 25));
+			len1 ++;
+		}
+		else{
+			status = putc(fd2, 'a' + (len2 % 25));
+			len2 ++;
+		}
+		
 	}
-	kprintf("status for putc: %d\n", status);
 
-	status = close(fd);
-	kprintf("status for close: %d\n", status);
-
-	// Open Another file
-	fd = open(LIFILESYS, "666.txt", "rw");
-	for (i = 0; i < 888888; i++)
-		putc(fd, 'Z');
-	close(fd);
+	status = close(fd1);
+	status = close(fd2);
 
 	// re-open the file for read
-	fd = open(LIFILESYS, "index.txt", "r");
-	kprintf("fd: %d\n", fd);
+	fd1 = open(LIFILESYS, "index.txt", "r");
+	fd2 = open(LIFILESYS, "content.txt", "r");
+	kprintf("fds: %d %d\n", fd1, fd2);
 
 	int pos;
-	for (i = 0; i < 100000; i++){
-		pos = rand() % 888888;
-		seek(fd, pos);
-		if (getc(fd) != 'A' + (pos % 25)){
-			kprintf("Error at position %d, quit!\n", pos);
-			exit();
+	char get_;
+	for (i = 0; i < 200000; i++){
+		pos = rand() % NUM_WRITTEN;
+		if (rand() & 1){
+			seek(fd1, pos);
+			get_ = getc(fd1);
+			if (get_ != 'A' + (pos % 25)){
+				kprintf("Error fd1 at position %d, quit!\n", pos);
+				kprintf("Should be %c, but get %c\n", 'A' + (pos % 25), get_);
+				exit();
+			}
+		}
+		else{
+			seek(fd2, pos);
+			get_ = getc(fd2);
+			if (get_ != 'a' + (pos % 25)){
+				kprintf("Error fd2 at position %d, quit!\n", pos);
+				kprintf("Should be %c, but get %c\n", 'a' + (pos % 25), get_);
+				exit();
+			}
 		}
 	}
 	
